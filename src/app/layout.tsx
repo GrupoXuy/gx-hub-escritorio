38f68f402d590abf91173c0f6ec4d857be7a1337
GrupoXuy <Silviobala@icloud.com>
2026-09-16 00:59:59 -0300

    feat: carregar acabamento visual das roupas do avatar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/next";
 import { Avatar3DStyles } from "@/components/avatar-3d";
 import "./globals.css";
+import "./avatar-clothing.css";
 const manrope = localFont({ src: "../../public/fonts/manrope.ttf", variable: "--font-manrope", display: "swap", weight: "200 800" });
 export const metadata: Metadata = {
   metadataBase: new URL("https://gxhubofficemeet.vercel.app"),
